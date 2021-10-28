01e8622e985a72724519d23e1814225a313fa9d5
5oN1A <82112240+5oN1A@users.noreply.github.com>
2021-10-28 14:48:05 +0200

    Hello Vue

---
+++ js/script.js
@@ -0,0 +1,10 @@
+Vue.config.devtools = true;
+
+var app = new Vue ({
+    el: "#root",
+    data:{
+        titolo: "Hello World!",
+        immagine: "https://www.mrw.it/img/cope/3775_1598907708.jpg",
+        altImg: "Logo Vue"
+    }
+});
